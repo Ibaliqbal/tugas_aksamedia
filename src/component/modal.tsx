a15574c564a15ac870802099f2ae2696177c692e
Ibaliqbal <iqbalmuthahhary@gmail.com>
2025-07-14 14:52:16 +0700

    style change opacity backdrop modal

--- src/component/modal.tsx
+++ src/component/modal.tsx
@@ -47,7 +47,7 @@ export const Modal = ({
     <div className="fixed inset-0 z-50 overflow-y-auto">
       {/* Backdrop */}
       <div
-        className="fixed inset-0 bg-black bg-opacity-50 transition-opacity"
+        className="fixed inset-0 bg-black opacity-50 transition-opacity"
         onClick={onClose}
       ></div>
 
